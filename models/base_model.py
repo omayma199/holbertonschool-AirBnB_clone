730c028444b6264573caf05e1491690099bfe5ab
omayma199 <5752@holbertonstudents.com>
2023-02-22 20:44:02 +0100

    base

--- models/base_model.py
+++ models/base_model.py
@@ -17,8 +17,6 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
-            models.storage.save()
         else:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
@@ -27,10 +25,20 @@ class BaseModel:
             for key, val in kwargs.items():
                 if "__class__" not in key:
                     setattr(self, key, val)
-    def __str__(self) -> str:
+    def __str__(self):
         return "[{}] ({}) {}".format(
             self.__class__.__name__,
             self.id,
             self.__dict__
         )
-    
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        
+    def to_dict(self):
+        """returns a dictionary containing all keys/values"""
+        dict = self.__dict__.copy()
+        dict['__class__'] = self.__class__.__name__
+        dict['updated_at'] = self.updated_at.isoformat()
+        dict['created_at'] = self.created_at.isoformat()
+        return dict
