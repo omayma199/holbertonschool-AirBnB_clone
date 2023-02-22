0fd79f5f9b9968b9a22e176fb857bdb073cebddb
omayma199 <5752@holbertonstudents.com>
2023-02-22 22:38:07 +0100

    init

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-"""
-    __init__ modules
-"""
-from .base_model import BaseModel
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
 storage = FileStorage()
 storage.reload()
