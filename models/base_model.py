28639fc0fea29375187ae401cc5af4c812e8aee2
omayma199 <5752@holbertonstudents.com>
2023-02-23 00:19:45 +0100

    base

--- models/base_model.py
+++ models/base_model.py
@@ -17,6 +17,8 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
+            models.storage.save()
         else:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
@@ -34,7 +36,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
-        
+        models.storage.save()
     def to_dict(self):
         """returns a dictionary containing all keys/values"""
         dict = self.__dict__.copy()
