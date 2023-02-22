3e76ffe22ce9ff252f636f8f09cff993ca1e1a2f
omayma199 <5752@holbertonstudents.com>
2023-02-23 00:42:08 +0100

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,12 @@
 """Create FileStorage Class"""
 import json
 from models.base_model import BaseModel
-
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class FileStorage:
 
