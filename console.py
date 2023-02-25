eabbb781f66f75b96a7a66b8990287a0a165ff17
omayma199 <omayma199@githb.com>
2023-02-25 15:58:00 +0100

    console

--- console.py
+++ console.py
@@ -4,6 +4,12 @@ import cmd
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 import models
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 storage = models.storage
@@ -14,7 +20,9 @@ class HBNBCommand (cmd.Cmd):
 
     prompt = '(hbnb) '
     classes = {
-               'BaseModel': BaseModel
+               'BaseModel': BaseModel, 'User': User, 'Place': Place,
+               'State': State, 'City': City, 'Amenity': Amenity,
+               'Review': Review
               }
 
     def emptyline(self):
