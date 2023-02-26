d163081e3117d765a66a4ca98ba1f44ec5e5e9d4
omayma199 <omayma199@githb.com>
2023-02-26 17:56:26 +0100

    test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,49 +1,95 @@
-"""Unit test for the class user"""
-
-import unittest
-from  models import user
-from models.user  import User
+#!/usr/bin/python3
+"""
+    Test Case For user Model and its Test
+"""
 from models.base_model import BaseModel
-from uuid import UUID
-from models import storage
-import datetime
+from models.user import User
+import unittest
+import inspect
+import time
+from datetime import datetime
+import pep8 as pcs
+from unittest import mock
+import models
 
-class TestUserClass(unittest.TestCase):
-    """TestCityClass test for the city class
-    Args:
-     unitest(): Propertys for unit testing
+
+class TestUser(unittest.TestCase):
+    """
+        unitesst for user class
     """
 
-    def set(self):
-        """Return  to class attribute"""
-        User.email= ""
-        User.password= ""
-        User.first_name= ""
-        User.last_name= ""
+    def issub_class(self):
+        """
+            test if User class is sub class of base model
+        """
+        user = User()
+        self.assertIsInstance(user, BaseModel)
+        self.assertTrue(hasattr(user, "id"))
+        self.assertTrue(hasattr(user, "created_at"))
+        self.assertTrue(hasattr(user, "update_at"))
 
-    def Test_User(self):
-        obj = User()
-        obj.name ="holbertonschool"
-        obj.my_number = 123
-        obj.save()
-        self.assertEqual(obj.name, "holbertonschool")
-        self.assertEqual((obj.my_number, 123))
-        self.assertEqual(isinstance(obj.__class__.__name__, "User"))
-        self.assertEqual(isinstance(obj.created_at, datetime), True)
-        self.assertEqual(isinstance(obj.updated_at, datetime), True)
-        self.assertEqual(type(obj.to_dict()), dict)
-    
-    def test_isinstance(self):
-        "Test if user is instance of BaseModel"
-        testUser = User()
-        self.assertTrue(isinstance(testUser, BaseModel))
-    
-    def test_attribyte_Type(self):
-        """"""
-        testUser = User()
-        self.assertTrue(type(testUser.email == str))
-        self.assertTrue(type(testUser.password ==str))
-        self.assertTrue(type(testUser.first_name ==str))
-        self.assertTrue(type(testUser.last_name ==str))
-if __name__ == '__main__':
-    unittest.main()
+    def test_email(self):
+        """
+            test class attribute email
+        """
+        user = User()
+        self.assertTrue(hasattr(user, "email"))
+        if models.storage_type == "db":
+            self.assertEqual(user.email, None)
+        else:
+            pass
+
+    def test_password(self):
+        """
+            test class attribute password
+        """
+        user = User()
+        self.assertTrue(hasattr(user, "password"))
+        if models.storage_type == "db":
+            self.assertEqual(user.password, None)
+        else:
+            pass
+
+    def test_name(self):
+        """
+            test class atribute first_name and last_name
+        """
+        user = User()
+        self.assertTrue(hasattr(user, "last_name"))
+        self.assertTrue(hasattr(user, "first_name"))
+        if models.storage_type == "db":
+            self.assertEqual(user.first_name, None)
+            self.assertEqual(user.last_name, None)
+        else:
+            pass
+
+    def test_to_dictUser(self):
+        """
+            test to dict method with user and the type
+            and content
+        """
+        user = User()
+        dict_cont = user.to_dict()
+        self.assertEqual(type(dict_cont), dict)
+        for attr in user.__dict__:
+            self.assertTrue(attr in dict_cont)
+            self.assertTrue("__class__" in dict_cont)
+
+    def test_dict_value(self):
+        """
+            test the returned dictionar values
+        """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        user = User()
+        dict_con = user.to_dict()
+        self.assertEqual(dict_con["__class__"], "User")
+        self.assertEqual(type(dict_con["created_at"]), str)
+        self.assertEqual(type(dict_con["updated_at"]), str)
+        self.assertEqual(
+            dict_con["created_at"],
+            user.created_at.strftime(time_format)
+        )
+        self.assertEqual(
+            dict_con["updated_at"],
+            user.updated_at.strftime(time_format))
+
