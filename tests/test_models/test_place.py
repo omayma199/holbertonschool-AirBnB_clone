a1e1f171267f6065158cb902b650185ef2f55ac3
omayma199 <omayma199@githb.com>
2023-02-26 19:16:22 +0100

    test

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,64 +1,61 @@
 #!/usr/bin/python3
-"""Unit test for the class place"""
-
+'''
+This is the 'test_place' module.
+test_amenity uses unittest to test the 'models/place' module.
+All credit for this module goes to Danton Rodriguez
+(https://github.com/p0516357)
+'''
 import unittest
-from  models import place
 from models.place import Place
-from models.base_model import BaseModel
-from uuid import UUID
-from models import storage
-from datetime import datetime
+import datetime
 
-class TestReviewClass(unittest.TestCase):
-    """TestCityClass test for the place class
-    Args:
-     unitest(): Propertys for unit testing
+
+class TestPlace(unittest.TestCase):
+    """Test for Place class
     """
 
-    def set(self):
-        """Return  to class attribute"""
-        Place.city_id= ""
-        Place.user_id= ""
-        Place.name= ""
-        Place.description= ""
-        Place.number_rooms= 0
-        Place.number_bathrooms= 0
-        Place.max_guest= 0
-        Place.price_by_night= 0
-        Place.latitude= 0.0
-        Place.longitude= 0.0
-        Place.amenity_ids= ""
-    def Test_Place(self):
-        obj = Place()
-        obj.name ="holbertonschool"
-        obj.my_number = 123
-        obj.save()
-        self.assertEqual(obj.name, "holbertonschool")
-        self.assertEqual((obj.my_number, 123))
-        self.assertEqual(isinstance(obj.__class__.__name__, "Place"))
-        self.assertEqual(isinstance(obj.created_at, datetime), True)
-        self.assertEqual(isinstance(obj.updated_at, datetime), True)
-        self.assertEqual(type(obj.to_dict()), dict)
-    
-    def test_isinstance(self):
-        "Test if user is instance of BaseModel"
-        testPlace = Place()
-        self.assertTrue(isinstance(testPlace, BaseModel))
-    
-    def test_attribyte_Type(self):
-        """"""
-        testPLace = Place()
-        self.assertTrue(type(testPLace.city_id == str))
-        self.assertTrue(type(testPLace.user_id ==str))
-        self.assertTrue(type(testPLace.name ==str))
-        self.assertTrue(type(testPLace.description == str))
-        self.assertTrue(type(testPLace.number_rooms == int))
-        self.assertTrue(type(testPLace.number_bathrooms ==int))
-        self.assertTrue(type(testPLace.max_guest == int))
-        self.assertTrue(type(testPLace.price_by_night == int))
-        self.assertTrue(type(testPLace.latitude == float)) 
-        self.assertTrue(type(testPLace.longitude == float)) 
-        self.assertTrue(type(testPLace.amenity_ids ==  list)) 
-                   
+    def setUp(self):
+        """sets up objects for testing later
+        """
+        self.test_model1 = Place()
+        self.test_model2 = Place()
+
+    def test_basic_setup(self):
+        """test for Place class attributes
+        """
+        self.assertTrue(hasattr(self.test_model1, "name"))
+        self.assertTrue(hasattr(self.test_model1, "city_id"))
+        self.assertTrue(hasattr(self.test_model2, "user_id"))
+        self.assertTrue(hasattr(self.test_model1, "number_rooms"))
+        self.assertTrue(hasattr(self.test_model1, "number_bathrooms"))
+        self.assertTrue(hasattr(self.test_model2, "latitude"))
+        self.assertTrue(hasattr(self.test_model1, "amenities"))
+        self.assertTrue(hasattr(self.test_model1, "latitude"))
+        self.assertTrue(self.test_model1.id != self.test_model2.id)
+
+    def test_types(self):
+        """tests whether isntance attribute types are correct
+        """
+        self.assertTrue(type(self.test_model1.amenities) is str)
+        self.assertTrue(type(self.test_model1.name) is str)
+        self.assertTrue(type(self.test_model1.city_id) is str)
+        self.assertTrue(type(self.test_model1.user_id) is str)
+        self.assertTrue(type(self.test_model1.description) is str)
+        self.assertTrue(type(self.test_model1.number_rooms) is int)
+        self.assertTrue(type(self.test_model1.number_bathrooms) is int)
+        self.assertTrue(type(self.test_model1.max_guest) is int)
+        self.assertTrue(type(self.test_model1.price_by_night) is int)
+        self.assertTrue(type(self.test_model1.longitude) is float)
+        self.assertTrue(type(self.test_model1.latitude) is float)
+
+    def test_save(self):
+        """testing whether save updates the updated_at attribute
+        """
+        m1u = self.test_model1.updated_at
+        self.test_model1.save()
+        m1u_saved = self.test_model1.updated_at
+        self.assertFalse(m1u == m1u_saved)
+
+
 if __name__ == '__main__':
     unittest.main()
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,48 +1,52 @@
 #!/usr/bin/python3
-"""Unit test for the class review"""
-
+'''
+This is the 'test_review' module.
+test_review uses unittest to test the 'models/review' module.
+All credit for this module goes to Danton Rodriguez
+(https://github.com/p0516357)
+'''
 import unittest
-from  models import review
-from models.review  import Review
-from models.base_model import BaseModel
-from uuid import UUID
-from models import storage
-from datetime import datetime
+from models.review import Review
+import datetime
 
-class TestReviewClass(unittest.TestCase):
-    """TestCityClass test for the city class
-    Args:
-     unitest(): Propertys for unit testing
+
+class TestReview(unittest.TestCase):
+    """Test for Review class
     """
 
-    def set(self):
-        """Return  to class attribute"""
-        Review.place_id= ""
-        Review.user_id= ""
-        Review.text= ""
-    def Test_Review(self):
-        obj = Review()
-        obj.name ="holbertonschool"
-        obj.my_number = 123
-        obj.save()
-        self.assertEqual(obj.name, "holbertonschool")
-        self.assertEqual((obj.my_number, 123))
-        self.assertEqual(isinstance(obj.__class__.__name__, "Review"))
-        self.assertEqual(isinstance(obj.created_at, datetime), True)
-        self.assertEqual(isinstance(obj.updated_at, datetime), True)
-        self.assertEqual(type(obj.to_dict()), dict)
-    
-    def test_isinstance(self):
-        "Test if user is instance of BaseModel"
-        testReview = Review()
-        self.assertTrue(isinstance(testReview, BaseModel))
-    
-    def test_attribyte_Type(self):
-        """"""
-        testReview = Review()
-        self.assertTrue(type(testReview.place_id == str))
-        self.assertTrue(type(testReview.user_id ==str))
-        self.assertTrue(type(testReview.text ==str))
-        
+    def setUp(self):
+        """sets up objects for testing later
+        """
+        self.test_model1 = Review()
+        self.test_model2 = Review()
+
+    def test_basic_setup(self):
+        """test for proper init of class
+        """
+        self.assertTrue(hasattr(self.test_model1, "place_id"))
+        self.assertTrue(hasattr(self.test_model1, "user_id"))
+        self.assertTrue(hasattr(self.test_model1, "text"))
+        self.assertTrue(self.test_model1.id != self.test_model2.id)
+        m1c = self.test_model1.place_id
+        m2c = self.test_model2.place_id
+        self.assertTrue(m1c == m2c)
+        self.assertTrue(type(m1c) is str)
+
+    def test_types(self):
+        """test for proper types of instance attributes
+        """
+        self.assertTrue(type(self.test_model1.place_id) is str)
+        self.assertTrue(type(self.test_model1.user_id) is str)
+        self.assertTrue(type(self.test_model1.text) is str)
+
+    def test_save(self):
+        """testing whether save updates the updated_at attribute
+        """
+        m1u = self.test_model1.updated_at
+        self.test_model1.save()
+        m1u_saved = self.test_model1.updated_at
+        self.assertFalse(m1u == m1u_saved)
+
+
 if __name__ == '__main__':
     unittest.main()
