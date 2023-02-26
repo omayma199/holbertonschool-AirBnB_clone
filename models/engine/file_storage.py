acbbcccc9960ff8fe87c1237733afae05b0941ea
omayma199 <omayma199@githb.com>
2023-02-26 16:49:46 +0100

    console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,7 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+
 class FileStorage:
 
     __file_path = "file.json"
@@ -18,7 +19,8 @@ class FileStorage:
         return (FileStorage.__objects)
 
     def new(self, obj):
-        FileStorage.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+        FileStorage.__objects["{}.{}".format(obj.__class__
+                                    .__name__, obj.id)] = obj
 
     def save(self):
         dict = {}
@@ -26,14 +28,17 @@ class FileStorage:
             dict[key] = value.to_dict()
         with open(self.__file_path, 'w', encoding="utf-8") as file:
             json.dump(dict, file)
-    
+
     def reload(self):
-        dict = {'BaseModel':BaseModel, 'User': User, 'Place': Place,
-        'State': State, 'City': City, 'Amenity': Amenity, 'Review': Review}
+        dict = {
+                'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                'State': State, 'City': City, 'Amenity': Amenity,
+                'Review': Review
+                }
         try:
             with open(self.__file_path, 'r') as file:
                 new_dict = json.load(file)
                 for key, value in new_dict.items():
-                   self.new(dict[value['__class__']](**value))
+                    self.new(dict[value['__class__']](**value))
         except FileNotFoundError:
             pass
