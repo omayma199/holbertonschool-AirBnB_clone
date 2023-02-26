6c8b47705f901a1ef517f760dbe37d92d5378ed7
omayma199 <omayma199@githb.com>
2023-02-26 13:23:36 +0100

    console

--- console.py
+++ console.py
@@ -101,7 +101,7 @@ class HBNBCommand (cmd.Cmd):
     def do_all(self, arg):
         List = arg.split()
         obj_list = []
-        if List[0] not in classes:
+        if List[0] not in self.classes:
             print("** class doesn't exist **")
         else:
             newList = []
